312f9a853166426ae1b8fb651182936f0b20d777
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-01-06 15:43:33 -0500

    update

--- update.sh
+++ update.sh
@@ -6,11 +6,11 @@ Rscript -e "rmarkdown::render('README.Rmd')"
 git add .
 git commit -m "betareg"
 git push
-git checkout gh-pages
-git checkout master -- csv
-git checkout master -- doc
-git checkout master -- datasets.csv
-git checkout master -- datasets.html
+git checkout gh-pages;
+git checkout master -- csv;
+git checkout master -- doc;
+git checkout master -- datasets.csv;
+git checkout master -- datasets.html;
 git add .
 git commit -m "betareg"
 git push
