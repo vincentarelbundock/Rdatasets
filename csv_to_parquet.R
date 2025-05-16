451b9a1cf0f490c3a5980e8470fa8f5c43ebdc72
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-05-15 21:49:10 -0400

    parquet

---
+++ csv_to_parquet.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+library(data.table)
+library(nanoparquet)
+library(fs)
+
+# Find all CSV files recursively in csv/
+csv_files <- dir(
+    'csv',
+    pattern = '\\.[cC][sS][vV]$',
+    recursive = TRUE,
+    full.names = TRUE
+)
+
+for (csv_path in csv_files) {
+    # Compute the corresponding parquet path
+    parquet_path <- sub('^csv/', 'parquet/', csv_path)
+    parquet_path <- sub('\\.[cC][sS][vV]$', '.parquet', parquet_path)
+
+    # Create output directory if it doesn't exist
+    dir_create(dirname(parquet_path))
+
+    # Read CSV and write Parquet
+    dt <- fread(csv_path)
+    write_parquet(dt, parquet_path)
+    cat(sprintf('Converted %s -> %s\n', csv_path, parquet_path))
+}
